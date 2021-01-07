7d52e0733a4fb91cae5a0adbdce61551f36dd7ca
Jordan Reed <jordanreed03@gmail.com>
2021-01-07 06:27:51 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,8 +3,9 @@ class Backer
   attr_accessor :backed_projects, :name
   
   def initialize(name)
+     @name = name
     @backed_projects = []
-    @name = name
+   
   end
 
   def back_project(project)
